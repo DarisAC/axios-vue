fc02feaccf7d6a140f3879a11fc519bcb8e166f4
Daris <vo14onoksteam@gmail.com>
2022-01-28 01:02:21 +0600

    1st try

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,52 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ul v-if=" posts.length != 0">
+      <li v-for="(index, post) of posts" :key="index">
+        <p><strong>{{ posts[post].category }}</strong></p>
+        <p>{{posts[post].title}}</p>
+        <p class="img" v-if="posts[post].image != undefined">{{
+          imagePost(posts[post].image)
+          }}</p>
+        <p>{{posts[post].rating.rate}}</p>
+        <p>{{posts[post].rating.count}}</p>
+      </li>
+    </ul>
+    <ul v-if="errors != 0">
+      <li v-for="(index, error) of errors" :key="index">
+        {{errors[error].message}}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+// `<img src=${posts[post].image}>`
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data: () => ({
+    posts: [],
+    errors:[]
+  }),
+
+  created() {
+    axios.get('https://fakestoreapi.com/products')
+    .then(response =>{
+      this.posts = response.data
+    })
+    .catch(e =>{
+      this.errors.push(e)
+    })
+  },
+  methods:{
+    imagePost(item) {
+      console.log(item);
+      return this.innerHTML =`<img src=${item}>`;
+    }
   }
 }
+// 'https://fakestoreapi.com/products'
 </script>
+
